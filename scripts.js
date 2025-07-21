b1d5417eda28a9543161fe1c96bcd6074fa8be07
droidsareut <droidsareut@proton.me>
2025-07-20 20:00:11 -0400

    feat: Add play round function to play game function

--- scripts.js
+++ scripts.js
@@ -12,39 +12,41 @@ function getHumanChoice() {
     return input
 };
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "Rock") {
-        if (computerChoice === "Scissors") {
-            console.log("YOU WIN!")
-            humanScore++;
-        } else if (computerChoice === "Paper") {
-            console.log("YOU LOSE!");
-            computerScore++;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "Rock") {
+            if (computerChoice === "Scissors") {
+                console.log("YOU WIN!")
+                humanScore++;
+            } else if (computerChoice === "Paper") {
+                console.log("YOU LOSE!");
+                computerScore++;
+            } else {
+                console.log("TIE!");
+            }
+        } else if (humanChoice === "Paper") {
+            if (computerChoice === "Scissors") {
+                console.log("YOU LOSE!")
+                computerScore++;
+            } else if (computerChoice === "Paper") {
+                console.log("TIE!");
+            } else {
+                console.log("YOU WIN!");
+                humanScore++;
+            }
         } else {
-            console.log("TIE!");
-        }
-    } else if (humanChoice === "Paper") {
-        if (computerChoice === "Scissors") {
-            console.log("YOU LOSE!")
-            computerScore++;
-        } else if (computerChoice === "Paper") {
-            console.log("TIE!");
-        } else {
-            console.log("YOU WIN!");
-            humanScore++;
-        }
-    } else {
-        if (computerChoice === "Scissors") {
-            console.log("TIE!")
-        } else if (computerChoice === "Paper") {
-            console.log("YOU WIN!");
-            humanScore++;
-        } else {
-            console.log("YOU LOSE!");
-            computerScore++;
+            if (computerChoice === "Scissors") {
+                console.log("TIE!")
+            } else if (computerChoice === "Paper") {
+                console.log("YOU WIN!");
+                humanScore++;
+            } else {
+                console.log("YOU LOSE!");
+                computerScore++;
+            }
         }
     }
 }
